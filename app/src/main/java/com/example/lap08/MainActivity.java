c54e613da3ff3503403c01e084d77ca32240999d
stoky71 <10lop96phungbuitronghieu@gmail.com>
2022-04-17 11:33:52 +0700

    sync error

---
+++ app/src/main/java/com/example/lap08/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lap08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button signbtn = findViewById(R.id.SignInBtn);
+        Button regisbtn = findViewById(R.id.RegisterBtn);
+        signbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                open1();
+            }
+        });
+        regisbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                open2();
+            }
+        });
+    }
+    public void open1(){
+        Intent intent = new Intent(MainActivity.this, signin.class);
+        startActivity(intent);
+    }
+    public void open2(){
+        Intent intent = new Intent(MainActivity.this, register.class);
+        startActivity(intent);
+    }
+}
